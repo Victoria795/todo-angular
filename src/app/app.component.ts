32cb8742f3d0e293310b3441f7da68dc5c85e431
victoria795 <vika.shintyabina.02@mail.ru>
2023-02-20 22:41:18 +0300

    Работающее приложение

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-angular';
+  
+  ngOnInit() {
+    this.getTasksFromStorage();
+  }
+
+  tasks: Array<{description: string, done: boolean}> = []
+
+  taskDescription = new FormControl('', Validators.required)
+  
+  error: boolean = false;
+  addTask(description: string) {
+    if (this.taskDescription.valid) {
+    this.tasks.unshift({
+      description,
+      done: false
+    });
+    this.taskDescription.reset();
+    this.setTasksToStorage();
+    this.error = false;}
+    else 
+    this.error = true;
+    return
+  }
+
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+    this.setTasksToStorage();
+  }
+
+  completeTask(index: number) {
+    this.tasks[index].done = !this.tasks[index].done;
+    this.setTasksToStorage();
+  }
+
+  getTasksFromStorage(){
+    let personalTasks = JSON.parse(localStorage.getItem('personalTasks')||'') || [];
+    this.tasks = personalTasks;
+};
+
+  setTasksToStorage(){
+  localStorage.setItem('personalTasks', JSON.stringify(this.tasks));
 }
+}
+
